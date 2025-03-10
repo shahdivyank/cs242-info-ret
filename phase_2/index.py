a0e2ce007969e55392a8b830f4fd0d17e64353fc
shahdivyank <divyank.shah.2016@gmail.com>
2025-03-09 23:30:09 -0700

    files

---
+++ phase_2/index.py
@@ -0,0 +1,50 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from langchain_community.vectorstores import FAISS
+from langchain_community.docstore.in_memory import InMemoryDocstore
+import json
+import numpy as np
+from datetime import datetime
+from faiss import IndexFlatL2
+
+bert_index = IndexFlatL2(768)
+
+bert = HuggingFaceEmbeddings(
+    model_name="google-bert/bert-base-uncased",
+)
+
+bert_vectorstore = FAISS(
+    embedding_function=bert,
+    index=bert_index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+TOTAL_DOCUMENTS = 137375
+
+times = np.zeros(TOTAL_DOCUMENTS)
+
+with open("../phase_1/indexing/data.json", "r", encoding="utf-8", errors="ignore") as contents:
+    jobs = json.load(contents)
+
+    times = np.zeros(TOTAL_DOCUMENTS)
+
+    start = datetime.now()
+
+    print("Finished Loading File.")
+
+    for index in range(TOTAL_DOCUMENTS):
+        job = jobs[index]
+
+        document = Document(
+            id = job["Application Link"],
+            page_content= job["Description"],
+            metadata={"title": job["Title"], "location": job["Location"]},
+        )
+
+        bert_vectorstore.add_documents(documents = [document])
+
+        times[index] = (datetime.now() - start).total_seconds()
+
+np.save('bert_embeddings_indexing.npy', times)
+bert_vectorstore.save_local("bert_index")
--- phase_2/website/api/index.py
+++ phase_2/website/api/index.py
@@ -1,18 +1,35 @@
 from flask import Flask, request
 from flask_cors import CORS
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-todos = []
-todo_id_counter = 1
+bert = HuggingFaceEmbeddings(
+    model_name="google-bert/bert-base-uncased",
+)
 
+vectorstore = FAISS.load_local(
+    "/Users/shahdivyank/Desktop/cs242-info-ret/phase_2/bert_index", bert, allow_dangerous_deserialization=True
+)
 
 @app.route("/api/query", methods=["GET"])
 def query():
     q = request.args.get('q')
 
-    return request.args
+    results = vectorstore.search(q, k=5, search_type="similarity")
+
+    output = []
+
+    for result in results:
+        output.append({
+            "metadata": result.metadata,
+            "page_content": result.page_content
+        })
+
+    return json.dumps(output)
 
 if __name__ == "__main__":
     app.run()
